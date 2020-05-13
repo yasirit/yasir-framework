b20a4ad1d2586d78132127b089662421ed7e6de8
shenyixin <shenyixin@rainbowcn.com>
2020-05-13 17:31:22 +0800

    redis FastJson序列化器

--- yasir-starter-redis/src/main/java/net/cn/yasir/framework/redis/RedisAutoConfiguration.java
+++ yasir-starter-redis/src/main/java/net/cn/yasir/framework/redis/RedisAutoConfiguration.java
@@ -1,8 +1,9 @@
 package net.cn.yasir.framework.redis;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+//import com.fasterxml.jackson.annotation.JsonAutoDetect;
+//import com.fasterxml.jackson.annotation.PropertyAccessor;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import net.cn.yasir.framework.redis.util.RedisUtil;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
@@ -41,25 +42,28 @@ public class RedisAutoConfiguration {
         // 配置连接工厂
         template.setConnectionFactory(factory);
 
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
-        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会抛异常
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jacksonSeial.setObjectMapper(om);
-
-        // 值采用json序列化
-        template.setValueSerializer(jacksonSeial);
+        //FastJson序列化器
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+//        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
+//        ObjectMapper om = new ObjectMapper();
+//        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会抛异常
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jacksonSeial.setObjectMapper(om);
+//        fastJsonRedisSerializer.
+//
+//        // 值采用json序列化
+//        template.setValueSerializer(jacksonSeial);
         //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
-
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(fastJsonRedisSerializer);
         // 设置hash key 和value序列化模式
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(jacksonSeial);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
         template.afterPropertiesSet();
-
         return template;
     }
 
